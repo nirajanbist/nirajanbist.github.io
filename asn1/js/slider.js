447f397c2cbe6bd2b80f0404a81ac61297e641a6
Neon <bistneon@gmail.com>
2021-05-11 16:27:46 +0545

    lo

--- asn1/js/slider.js
+++ asn1/js/slider.js
@@ -1,6 +1,10 @@
 var myDivs = document.getElementsByClassName("carousel-image-wrapper");
 var outerDivs = document.getElementsByClassName("carousel-container");
-console.log(myDivs)
+var cStyle=window.getComputedStyle(myDivs[0]);
+var containerWidth=parseInt(cStyle.getPropertyValue('width'))
+
+
+console.log(containerWidth)
 var timer;
 var currentIndex=0;
 function leftrightButtons(elem){
@@ -8,10 +12,6 @@ function leftrightButtons(elem){
   elem.rightbtn=document.createElement('div');
   elem.leftbtn.classList.add('lbtn')
   elem.rightbtn.classList.add('rbtn')
-  // elem.leftbtn.addEventListener('click',function(e){
-  //   currentIndex-=1
-  //   console.log(elem.childNodes[0])
-  // })
   elem.append(elem.leftbtn);
   elem.append(elem.rightbtn);
 }
@@ -26,15 +26,17 @@ function dotmaker(cnt,parent){
     dot.index=i;
     parent.dots.push(dot)
     dot.classList.add('dot')
-    if (i===0){ parent.lastActive=dot; dot.classList.add('active-dot')}
+    if (i===0){ 
+      parent.lastActive=dot
+      dot.classList.add('active-dot')
+    }
     dot.addEventListener('click',function(e){
-      parent.left=-e.target.index*1000
-      parent.style.left=-e.target.index*1000+'px'
+      parent.left=-e.target.index*containerWidth
+      parent.style.left=-e.target.index*containerWidth+'px'
       e.target.classList.add('active-dot')
       parent.lastActive.classList.remove('active-dot')
       parent.lastActive=e.target
-      
-
+      currentIndex=e.target.index
     })
     this.elem.appendChild(dot)
   }
@@ -54,40 +56,46 @@ for (var i = 0; i < myDivs.length; i++) {
   });
   element.parentElement.rightbtn.addEventListener('click',function(event){
     currentIndex+=1
-    goRight(element)
+    goRight(event.target.parentElement.wrapper)
     
 });
 }
 for (var i = 0; i < outerDivs.length; i++) {
   outerDivs[i].append(dotmaker(myDivs[i].imageCount,myDivs[i]))
+  outerDivs[i].wrapper=myDivs[i]
   
   
 }
 function goLeft(elem){
-  elem.lastActive=parent.dots[currentIndex]
-  elem.lastActive.classList.remove('active-dot')
+
+  
   timer = setInterval(function() {
     elem.style.left=elem.left+10+'px';
     elem.left=elem.left+10; 
     //reset frame to last image
     if(currentIndex<0) {
       currentIndex=elem.imageCount-1;
-      elem.left=-currentIndex*1000;
+      elem.left=-currentIndex*containerWidth;
       elem.style.left=elem.left+'px';
       clearInterval(timer) 
     }
     // after sliding anim
-    else if (elem.left > -currentIndex*1000-10){
+    else if (elem.left > -currentIndex*containerWidth-10){
       
-      elem.left=-currentIndex*1000;
+      elem.left=-currentIndex*containerWidth;
       elem.style.left=elem.left+'px';
       clearInterval(timer)   
     }
   }, 5);
-
+  elem.lastActive.classList.remove('active-dot')
+  var dotlen=elem.dots.length
+  elem.lastActive=elem.dots[(currentIndex+dotlen)%dotlen]
+  console.log(currentIndex)
+  elem.lastActive.classList.add('active-dot')
 }
 
 function goRight(elem){
+ 
   timer = setInterval(function() {
     elem.style.left=elem.left-10+'px';
     elem.left=elem.left-10; 
@@ -99,12 +107,17 @@ function goRight(elem){
       clearInterval(timer);
     }
     // after sliding animation
-    else if (elem.left < -currentIndex*1000-10){
+    else if (elem.left < -currentIndex*containerWidth-10){
       
-      elem.left=-currentIndex*1000;
+      elem.left=-currentIndex*containerWidth;
       elem.style.left=elem.left+'px';
       clearInterval(timer)   
     }
   }, 5);
+  
+  elem.lastActive.classList.remove('active-dot')
+  elem.lastActive=elem.dots[currentIndex%elem.dots.length]
+  console.log(currentIndex)
+  elem.lastActive.classList.add('active-dot')
 }
   
